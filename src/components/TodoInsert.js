1f3681fc0faf215546795de64a01abb3ec459188
bbAn <damaskrose83@gmail.com>
2023-01-05 21:30:51 +0900

    10.3.2.4 TodoInsert에서 onSubmit 이벤트 설정

--- src/components/TodoInsert.js
+++ src/components/TodoInsert.js
@@ -2,7 +2,7 @@ import React, { useState, useCallback } from 'react';
 import { MdAdd } from 'react-icons/md';
 import './TodoInsert.scss';
  
-const TodoInsert = () => {
+const TodoInsert = ({ onInsert }) => {
   const [value, setValue] = useState("");
 
   const onChange = useCallback(e=> { 
@@ -10,8 +10,21 @@ const TodoInsert = () => {
     setValue(e.target.value);
   }, []);
 
+  const onSubmit = useCallback(
+    e=> {
+      onInsert(value); // props로 받아 온 onInsert 함수에 현재 value 값을 파라미터로 넣어서 호출
+      setValue(""); // value 값 초기화
+      
+      // submit 이벤트는 브라우저에서 새로고침을 발생 시킴
+      // 이를 방지하기 위해 사용하는 함수
+      e.preventDefault();
+    },
+    [onInsert, value],
+  );
+
   return (
-    <form className="TodoInsert">
+    <form className="TodoInsert" onSubmit={onSubmit}>
+      {/* onSubmit 대신에 버튼의 onClick 이벤트로도 처리할 수 있음 */}
       <input 
         placeholder="할 일을 입력하세요" 
         value={value}
