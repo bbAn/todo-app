e40308fcac52a6a176c3f190f1bbd99d377060c0
bbAn <damaskrose83@gmail.com>
2023-01-29 15:42:59 +0900

    11.5.2 useReducer 사용하기

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useCallback } from 'react';
+import React, { useReducer, useRef, useCallback } from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
@@ -16,9 +16,26 @@ function createBulkTodos() {
   return array;
 }
 
+function todoReducer(todos, action) {
+  switch(action.type) {
+    case 'INSERT': // 새로추가
+      // { type: 'INSERT', todo: { id: 1, text: 'todo', checked: false }}
+      return todos.concat(action.todo);
+    case 'REMOVE': // 제거
+      // { type: 'REMOVE', id: 1 }
+      return todos.filter(todo => todo.id !== action.id);
+    case 'TOGGLE': // 토글
+      // {type: 'REMOVE', id: 1}
+      return todos.map(todo => 
+        todo.id === action.id ? { ...todo, checked: !todo.checked } : todo,
+      );
+    default:
+      return todos;
+  }
+}
+
 const App = () => {
- const [todos, setTodos] = useState(createBulkTodos); 
- // useState(createBulkTodos)처럼 파라미터를 함수 형태로 넣어 주면 컴포넌트가 처음 렌더링될 때만 createBulkTodos 함수가 실행
+  const [todos, dispatch] = useReducer(todoReducer, undefined, createBulkTodos);
 
   // 고윳값으로 사용될 id
   // ref를 사용하여 변수 담기
@@ -31,29 +48,19 @@ const App = () => {
         text,
         checked: false,
       };
-      setTodos(todos => todos.concat(todo)); // 어떻게 업데이트할지 정의해 주는 업데이트 함수를 넣어 줌. setTodos를 사용할 때 그 안에 todos =>만 앞에 넣어줌
+      dispatch({ type: 'INSERT', todo });
       nextId.current += 1; // nextId 1씩 더하기
-    },
-    [],
+    }, [],
   );
 
-  const onRemove = useCallback(
-    id => {
-      setTodos(todos => todos.filter(todo => todo.id !== id));
-    },
-    [],
+  const onRemove = useCallback(id => {
+    dispatch({ type: 'REMOVE', id });
+  }, [],
   );
 
-  const onToggle = useCallback(
-    id => {
-      setTodos(todos =>
-        todos.map(todo =>  // 불변성을 유지하면서 특정 배열 원소를 업데이트해야 할 때 map을 사용하면 짧은 코드로 쉽게 작성 
-          todo.id === id ? { ...todo, checked: !todo.checked } : todo,
-        ),
-      );
-    },
-    [],
-  );
+  const onToggle = useCallback(id=> {
+    dispatch({ type: 'TOGGLE', id });
+  }, []);
 
   return (
     <TodoTemplate>
